16ad9d8a2e7fd6617f4616d4e62bbdbfe0c3f2cc
Yulia <parma2903@gmail.com>
2019-06-13 08:32:15 +0300

    Задание 3

---
+++ js/setup.js
@@ -0,0 +1,54 @@
+'use strict';
+var userDialog = document.querySelector('.setup');
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var numberOfWizards = 4;
+var wizards = [];
+
+userDialog.classList.remove('hidden');
+
+function getElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function createWizard(names, surnames, coatColors, eyesColors) {
+  var result = {
+    name: getElement(names) + ' ' + getElement(surnames),
+    coatColor : getElement(coatColors),
+    eyesColor : getElement(eyesColors)
+  }
+  return result
+}
+
+function createWizards(num) {
+  for (var i = 0; i < num; i++) {
+    wizards.push(createWizard(NAMES, SURNAMES, COAT_COLORS, EYES_COLORS));
+  }
+  return wizards
+}
+createWizards(numberOfWizards);
+
+console.log(wizards);
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+}
+
+var fragment = document.createDocumentFragment();
+
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+
+similarListElement.appendChild(fragment);
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
