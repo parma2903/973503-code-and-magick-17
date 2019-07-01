fe29fd8fbd86e038c76c62ec411a1db8c699a086
Yulia <parma2903@gmail.com>
2019-07-01 09:14:58 +0300

    Удалила моки

--- js/wizards.js
+++ js/wizards.js
@@ -5,9 +5,8 @@
   var setupSimilar = document.querySelector('.setup-similar');
   var similarListElement = window.setup.querySelector('.setup-similar-list');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-  var wizardsData = createWizards(4);
 
-  window.backend.load(getWizard(wizardsData), window.backend.errorHandler);
+  window.backend.load(getWizard, window.backend.errorHandler);
 
   function getWizard(wizards) {
     var fragment = document.createDocumentFragment();
@@ -20,29 +19,12 @@
     setupSimilar.classList.remove('hidden');
   }
 
-  function createWizard(names, surnames, coatColors, eyesColors) {
-    var result = {
-      name: window.util.getElement(names) + ' ' + window.util.getElement(surnames),
-      coatColor: window.util.getElement(coatColors),
-      eyesColor: window.util.getElement(eyesColors)
-    };
-    return result;
-  }
-
-  function createWizards(num) {
-    var wizardsArray = [];
-    for (var k = 0; k < num; k++) {
-      wizardsArray.push(createWizard(window.wizardData.NAMES, window.wizardData.SURNAMES, window.wizardData.COAT_COLORS, window.wizardData.EYES_COLORS));
-    }
-    return wizardsArray;
-  }
-
   function renderWizard(wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColors;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
 
     return wizardElement;
   }
